0f68fc0daa43915f2b71d3686edf4f40148b50c7
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-05-08 20:53:27 +0900

    make health chcker

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use actix_web::{web, App,HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req:HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    //HttpResponse::Ok().finish()
+    // HttpResponse::Ok()
+    HttpResponse::Ok().body("Server is alive!")
 }
 
 #[tokio::main]
@@ -10,7 +16,7 @@ async fn main() -> std::io::Result<()> {
     println!("Server started");
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
